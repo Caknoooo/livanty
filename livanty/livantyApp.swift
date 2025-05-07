430e83e12421c08ef418cbc602bac1d2b093581d
Caknoooo <caknocomel@gmail.com>
2025-05-07 16:55:02 +0700

    feat: initial commit

---
+++ livanty/livantyApp.swift
@@ -0,0 +1,17 @@
+//
+//  livantyApp.swift
+//  livanty
+//
+//  Created by M Naufal Badruttamam on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct livantyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
